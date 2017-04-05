e9961a89bd3e9e5834e65cd6b29eb20108b3b245
Helge Pettersen <helge.pettersen@ift.uib.no>
2017-04-05 14:01:12 +0200

    Updated another runDegrader.sh file to parallel sequential run type

--- projects/calculateEnergySpectrumAfterWaterDegrader/runDegrader.sh
+++ projects/calculateEnergySpectrumAfterWaterDegrader/runDegrader.sh
@@ -3,6 +3,9 @@
 echo "Usage: ./run.sh <degraderthickness_from> <degraderthickness_increment> <degraderthickness_to>"
 echo Phantom thickness from $1 step $2 to $3
 
+NCORES=8
+IDX=1
+
 if [ $# -ne 3 ]; then
 	echo Invalid number of arguments: $#
 	exit
@@ -14,4 +17,11 @@ do
    beampos=`echo "scale=3; -$i-5" | bc`
 	nice -n 10 Gate -a "'[energy,250] [degraderthickness,$i] [halfdegraderthickness,$hdt] [beampos,$beampos]'" waterphantom.mac > terminal_output.txt &
 	echo "Running: $i"
+   if (( $IDX % $NCORES == 0 )); then
+      echo "Waiting for run $i (PID $!)"
+      IDX=1
+      time wait $!
+   else
+      IDX=$((IDX+1))
+   fi
 done
