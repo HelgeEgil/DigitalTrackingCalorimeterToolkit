b121ba34b070daccb7ddc5ef3f1a82f1e02c51e4
Helge Pettersen <helge.pettersen@ift.uib.no>
2018-08-14 13:24:20 +0200

    Added MC-MLP plots plus better analysis for MLP

--- projects/MLP/findMLP.C
+++ projects/MLP/findMLP.C
@@ -6,6 +6,7 @@
 #include <fstream>
 #include <TGraph.h>
 #include <TSpline.h>
+#include <TStyle.h>
 #include <Math/Vector3D.h>
 
 using namespace std;
@@ -24,27 +25,51 @@ XYZVector SplineMLP(Double_t t, XYZVector X0, XYZVector X1, XYZVector P0, XYZVec
    return S;
 }
 
-void findMLP(Float_t sigmaScaleFactor = 1, Float_t vectorScaleFactor = 0) {
+void findMLP(Float_t sigmaScaleFactor = 0.60, Float_t vectorScaleFactor = -5.85) {
    TFile *f = new TFile("MC/simpleScanner.root");
    TTree *tree = (TTree*) f->Get("Hits");
 
    Float_t     initialEnergy = 230;
+   Int_t       eventsToUse = 1000;
    Float_t     x, y, z, edep, sum_edep = 0, residualEnergy = 0;
    Int_t       eventID, parentID, lastEID = -1;
-   XYZVector   Xp0, Xp1, Xp2, Xp3, X0, X1, X0est, P0, P1, P0hat, P1hat, S; // Xp are the plane coordinates, X are the tracker coordinates (X1 = (Xp1 + Xp2) / 2)
+   XYZVector   Xp0, Xp1, Xp2, Xp3, X0, X1, X0est, X0err, X0NoTrk, P0, P0NoTrk, P1, P0hat, P1hat, S; // Xp are the plane coordinates, X are the tracker coordinates (X1 = (Xp1 + Xp2) / 2)
    Float_t     wepl, wet, Lambda0, Lambda1;
    ifstream    in;
+   TSpline3  * splineMCx, * splineMCy, * splineMLPx, * splineMLPy;
+   TSpline3  * splineMLPNoTrkx, * splineMLPNoTrky, * splineMLPestx, * splineMLPesty;
+   Double_t    arSplineMCx[1000], arSplineMCy[1000], arSplineMCz[1000], arSplineMLPx[1000], arSplineMLPy[1000], arSplineMLPz[1000];
+   Double_t    arSplineMLPNoTrkx[1000], arSplineMLPNoTrky[1000], arSplineMLPestx[1000], arSplineMLPesty[1000];
+   Int_t       idxSplineMC = 0, idxSplineMLP = 0;
+   Double_t    differenceArrayZ[1000];
+   Double_t    differenceArrayDiff[1000] = {};
+   Double_t    differenceArrayDiffNoTrk[1000] = {};
+   Double_t    differenceArrayDiffest[1000] = {};
+   Int_t       nInDifferenceArray = 0;
+   Int_t       idxDifferenceArray = 0;
    Int_t       idxWater = 0;
    Double_t    energiesWater[500], rangesWater[500];
    Float_t     energy, range;
    Float_t     sigmaFilter = 128.47;
-   Double_t    aPosMCx[100000];
-   Double_t    aPosMCz[100000];
-   Double_t    aPosMLPx[100000];
-   Double_t    aPosMLPz[100000];
+   Double_t    aPosMCx[10000], aPosMCz[10000];
+   Double_t    aPosMLPx[10000], aPosMLPz[10000];
+   Double_t    aPosMLPNoTrkx[10000];
+   Double_t    aPosMLPestx[10000];
    Int_t       aIdxMC = 0;
    Int_t       aIdxMLP = 0;
-   Bool_t      stop = false;
+   Bool_t      stop = false, stopacc = false;
+
+   gStyle->SetTitleFont(22);
+   gStyle->SetLabelFont(22);
+   gStyle->SetTextFont(22);
+   gStyle->SetLabelFont(22, "Y");
+   gStyle->SetTitleFont(22, "Y");
+   gStyle->SetTitleYOffset(1);
+   gStyle->SetLabelSize(0.045);
+   gStyle->SetLabelSize(0.045, "Y");
+   gStyle->SetTitleSize(0.045);
+   gStyle->SetTitleSize(0.045, "Y");
+   gStyle->SetTextSize(0.045);
 
    // Load Energy <-> Range spline
    in.open("Data/WaterPSTAR.csv");
@@ -67,9 +92,13 @@ void findMLP(Float_t sigmaScaleFactor = 1, Float_t vectorScaleFactor = 0) {
    tree->SetBranchAddress("edep", &edep);
    tree->SetBranchAddress("eventID", &eventID);
    tree->SetBranchAddress("parentID", &parentID);
+   
+   X0NoTrk.SetCoordinates(0,0, -115);
+   P0NoTrk.SetCoordinates(0, 0, 1); // Normalized
 
    for (Int_t i=0; i<tree->GetEntries(); ++i) {
-//      if (eventID > 1000) stop = true;
+      if (eventID > eventsToUse) stop = true;
+      if (eventID > 5) stopacc = true;
 
       tree->GetEntry(i);
 
@@ -92,10 +121,11 @@ void findMLP(Float_t sigmaScaleFactor = 1, Float_t vectorScaleFactor = 0) {
             P0hat = P0.Unit();
             P1hat = P1.Unit();
 
-            X0est = X1 * sigmaScaleFactor + P1 * vectorScaleFactor - X0;
+            X0est = X1 * sigmaScaleFactor + P1 * vectorScaleFactor;
+            X0err = X1 * sigmaScaleFactor + P1 * vectorScaleFactor - X0;
 
             hErrorNaive->Fill(X0.X(), X0.Y());
-            hErrorSigmaScale->Fill(X0est.X(), X0est.Y());
+            hErrorSigmaScale->Fill(X0err.X(), X0err.Y());
             
             // calculate Spline
             wepl = splineWater->Eval(initialEnergy);
@@ -107,10 +137,60 @@ void findMLP(Float_t sigmaScaleFactor = 1, Float_t vectorScaleFactor = 0) {
 
             for (Float_t t=0; t<1; t += 0.01) {
                S = SplineMLP(t, X0, X1, P0hat, P1hat, Lambda0, Lambda1);
-               if (eventID < 10) {
-                  aPosMLPx[aIdxMLP] = S.X();
-                  aPosMLPz[aIdxMLP++] = S.Z();
+               if (!stopacc) {
+                  aPosMLPx[aIdxMLP] = S.X(); // Collect many tracks 10 times
+                  aPosMLPz[aIdxMLP] = S.Z();
                }
+
+               arSplineMLPx[idxSplineMLP] = S.X(); // Collect a single track for all eventIDs
+               arSplineMLPy[idxSplineMLP] = S.Y();
+               arSplineMLPz[idxSplineMLP] = S.Z();
+
+               S = SplineMLP(t, X0NoTrk, X1, P0NoTrk, P1hat, Lambda0, Lambda1);
+               arSplineMLPNoTrkx[idxSplineMLP] = S.X();
+               arSplineMLPNoTrky[idxSplineMLP] = S.Y();
+
+               if (!stopacc) {
+                  aPosMLPNoTrkx[aIdxMLP] = S.X();
+               }
+
+               S = SplineMLP(t, X0est, X1, P0NoTrk, P1hat, Lambda0, Lambda1);
+               arSplineMLPestx[idxSplineMLP] = S.X();
+               arSplineMLPesty[idxSplineMLP++] = S.Y();
+               
+               if (!stopacc) {
+                  aPosMLPestx[aIdxMLP++] = S.X();
+               }
+            }
+         
+            // Compare MC and MLP here
+            // 1) Make splines to compare at same Z
+            splineMCx  = new TSpline3("splineMCx", arSplineMCz, arSplineMCx, idxSplineMC);
+            splineMCy  = new TSpline3("splineMCy", arSplineMCz, arSplineMCy, idxSplineMC);
+            splineMLPx = new TSpline3("splineMLPx", arSplineMLPz, arSplineMLPx, idxSplineMLP);
+            splineMLPy = new TSpline3("splineMLPy", arSplineMLPz, arSplineMLPy, idxSplineMLP);
+            splineMLPNoTrkx = new TSpline3("splineMLPNoTrkx", arSplineMLPz, arSplineMLPNoTrkx, idxSplineMLP);
+            splineMLPNoTrky = new TSpline3("splineMLPNoTrky", arSplineMLPz, arSplineMLPNoTrky, idxSplineMLP);
+            splineMLPestx = new TSpline3("splineMLPestx", arSplineMLPz, arSplineMLPestx, idxSplineMLP);
+            splineMLPesty = new TSpline3("splineMLPesty", arSplineMLPz, arSplineMLPesty, idxSplineMLP);
+
+            // 2) Sweep Z values and add the absolute difference to an array
+            Double_t diff_x, diff_y;
+            idxDifferenceArray = 0; // Sweep each time
+            nInDifferenceArray++; // To average at end
+            for (Double_t zSweep = -110; zSweep <= 110; zSweep += 2) { // Keep Sweep increment high to speed up!
+               diff_x = fabs(splineMCx->Eval(zSweep) - splineMLPx->Eval(zSweep));
+               diff_y = fabs(splineMCy->Eval(zSweep) - splineMLPy->Eval(zSweep));
+               differenceArrayZ[idxDifferenceArray] = zSweep;
+               differenceArrayDiff[idxDifferenceArray] += sqrt(pow(diff_x, 2) + pow(diff_y, 2));
+
+               diff_x = fabs(splineMCx->Eval(zSweep) - splineMLPNoTrkx->Eval(zSweep));
+               diff_y = fabs(splineMCy->Eval(zSweep) - splineMLPNoTrky->Eval(zSweep));
+               differenceArrayDiffNoTrk[idxDifferenceArray] += sqrt(pow(diff_x, 2) + pow(diff_y, 2));
+
+               diff_x = fabs(splineMCx->Eval(zSweep) - splineMLPestx->Eval(zSweep));
+               diff_y = fabs(splineMCy->Eval(zSweep) - splineMLPesty->Eval(zSweep));
+               differenceArrayDiffest[idxDifferenceArray++] += sqrt(pow(diff_x, 2) + pow(diff_y, 2));
             }
          }
          
@@ -120,12 +200,13 @@ void findMLP(Float_t sigmaScaleFactor = 1, Float_t vectorScaleFactor = 0) {
          sum_edep = edep;
          residualEnergy = 0;
          lastEID = eventID;
+         idxSplineMLP = 0;
+         idxSplineMC = 0;
       }
 
       else { // Still following the same particle
          sum_edep += edep;
       }
-
 
       if (parentID == 0) {
          if       (z < -119 && z > -121) { Xp0.SetCoordinates(x,y,z); }
@@ -133,15 +214,24 @@ void findMLP(Float_t sigmaScaleFactor = 1, Float_t vectorScaleFactor = 0) {
          else if  (z >  109 && z <  111) { Xp2.SetCoordinates(x,y,z); }
          else if  (z >  119 && z <  121) { Xp3.SetCoordinates(x,y,z); }
          else if  (z >  130)             { residualEnergy += edep; }
-         if (z < 130 && eventID < 10) {
+         if (z < 130 && !stopacc) {
             aPosMCx[aIdxMC] = x;
             aPosMCz[aIdxMC++] = z;
+         }
+         if (z < 130) {
+            arSplineMCx[idxSplineMC] = x;
+            arSplineMCy[idxSplineMC] = y;
+            arSplineMCz[idxSplineMC++] = z;
          }
       }
    }
 
    hResEnergy->Draw();
 
+
+   TCanvas *c1 = new TCanvas("c1", "MLP estimation", 1500, 600);
+   c1->Divide(3, 1, 0.001, 0.001);
+   c1->cd(1);
    TGraph *gMC = new TGraph(aIdxMC, aPosMCz, aPosMCx);
    TGraph *gMLP = new TGraph(aIdxMLP, aPosMLPz, aPosMLPx);
    gMC->SetMarkerStyle(7);
@@ -150,13 +240,73 @@ void findMLP(Float_t sigmaScaleFactor = 1, Float_t vectorScaleFactor = 0) {
    gMLP->SetMarkerColor(kBlue);
    gMC->Draw("AP");
    gMLP->Draw("P");
-   gMC->SetTitle("Optimized Spline MLP estimation (no nucl. interactions);Depth [mm];Lateral position [mm]");
+   gMC->SetTitle("Perfect knowledge;Depth [mm];Lateral position [mm]");
 
-   TCanvas *c = new TCanvas("c", "Beam spot estimation", 800, 600);
+   c1->cd(2);
+   TGraph *gMCNoTrk = new TGraph(aIdxMC, aPosMCz, aPosMCx);
+   TGraph *gMLPNoTrk = new TGraph(aIdxMLP, aPosMLPz, aPosMLPNoTrkx);
+   gMLPNoTrk->SetMarkerStyle(7);
+   gMLPNoTrk->SetMarkerColor(kBlue);
+   gMCNoTrk->SetMarkerStyle(7);
+   gMCNoTrk->SetMarkerColor(kRed);
+   gMCNoTrk->Draw("AP");
+   gMLPNoTrk->Draw("P");
+   gMCNoTrk->SetTitle("No Front Tracker - use (0,0);Depth [mm];Lateral position [mm]");
+
+   c1->cd(3);
+   TGraph *gMCest = new TGraph(aIdxMC, aPosMCz, aPosMCx);
+   TGraph *gMLPest = new TGraph(aIdxMLP, aPosMLPz, aPosMLPestx);
+   gMLPest->SetMarkerStyle(7);
+   gMLPest->SetMarkerColor(kBlue);
+   gMCest->SetMarkerStyle(7);
+   gMCest->SetMarkerColor(kRed);
+   gMCest->Draw("AP");
+   gMLPest->Draw("P");
+   gMCest->SetTitle("No Front Tracker - estimate PB;Depth [mm];Lateral position [mm]");
+
+   TCanvas *c = new TCanvas("c", "Beam spot estimation", 1000, 600);
    c->Divide(2, 1, 0.001, 0.001);
 
    c->cd(1);
    hErrorNaive->Draw("COLZ");
    c->cd(2);
    hErrorSigmaScale->Draw("COLZ");
+
+
+   printf("Normalizing the %d indexes of differenceArray from %d.\n", idxDifferenceArray, nInDifferenceArray);
+   for (Int_t i=0; i<idxDifferenceArray; i++) {
+      differenceArrayDiff[i] /= nInDifferenceArray;
+      differenceArrayDiffNoTrk[i]  /= nInDifferenceArray;
+      differenceArrayDiffest[i] /= nInDifferenceArray;
+      differenceArrayZ[i] += 110;
+   }
+
+   TCanvas *c2 = new TCanvas("c2", "MC vs MLP difference", 1500, 600);
+   c2->Divide(3,1,0.001,0.001);
+   c2->cd(1);
+   TGraph *gDifferencePerfect = new TGraph(idxDifferenceArray, differenceArrayZ, differenceArrayDiff);
+   gPad->SetGridy();
+   gDifferencePerfect->SetTitle("Perfect knowledge;Depth in phantom [mm];Error MLP - MC [mm]");
+   gDifferencePerfect->SetLineColor(kRed);
+   gDifferencePerfect->SetLineWidth(2);
+   gDifferencePerfect->Draw("AL");
+   gDifferencePerfect->GetYaxis()->SetRangeUser(0, 4);
+
+   c2->cd(2);
+   TGraph *gDifferenceNoTrk = new TGraph(idxDifferenceArray, differenceArrayZ, differenceArrayDiffNoTrk);
+   gPad->SetGridy();
+   gDifferenceNoTrk->SetTitle("No Front Tracker - use (0,0);Depth in phantom [mm]; Error MLP - MC [mm]");
+   gDifferenceNoTrk->SetLineColor(kRed);
+   gDifferenceNoTrk->SetLineWidth(2);
+   gDifferenceNoTrk->Draw("AL");
+   gDifferenceNoTrk->GetYaxis()->SetRangeUser(0, 4);
+
+   c2->cd(3);
+   TGraph *gDifferenceest = new TGraph(idxDifferenceArray, differenceArrayZ, differenceArrayDiffest);
+   gPad->SetGridy();
+   gDifferenceest->SetTitle("No Front Tracker - estimate PB;Depth in phantom [mm]; Error MLP - MC [mm]");
+   gDifferenceest->SetLineColor(kRed);
+   gDifferenceest->SetLineWidth(2);
+   gDifferenceest->Draw("AL");
+   gDifferenceest->GetYaxis()->SetRangeUser(0, 4);
 }
