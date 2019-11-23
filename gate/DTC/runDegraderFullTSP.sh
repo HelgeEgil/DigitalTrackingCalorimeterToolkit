df48df87d6755ea0923e00c870623d9f1636eeb3
Helge E. S. Pettersen <helge@krnmpi0.hvl.no>
2019-11-23 20:39:15 +0100

    Added Final-geometry calibration

--- gate/DTC/runDegraderFullTSP.sh
+++ gate/DTC/runDegraderFullTSP.sh
@@ -3,12 +3,10 @@
 NCORES=8
 IDX=1
 
-for j in `seq -w 35 1 35`; do
-   for i in `seq -w 5 5 330`; do
-      hdt=`echo "scale=3; -$i/2-2" | bc`
-      beampos=`echo "scale=3; -$i-5" | bc`
-      cpulimit -l 70 Gate -a "'[absorberthickness,$j] [energy,917] [degraderthickness,$i] [halfdegraderthickness,$hdt] [beampos,$beampos]'" Main.mac > terminal_output.txt &
-      PIDLIST="$PIDLIST $!"
-      echo "Running (full): Absorber thickness = $j mm; degrader thickness = $i mm"
-   done
+for i in `seq -w 274 4 330`; do
+   hdt=`echo "scale=3; -$i/2-2" | bc`
+   beampos=`echo "scale=3; -$i-5" | bc`
+   tsp Gate -a "'[energy,917] [degraderthickness,$i] [halfdegraderthickness,$hdt] [beampos,$beampos]'" Main.mac
+   PIDLIST="$PIDLIST $!"
+   echo "Running: Absorber thickness = $j mm; degrader thickness = $i mm"
 done
