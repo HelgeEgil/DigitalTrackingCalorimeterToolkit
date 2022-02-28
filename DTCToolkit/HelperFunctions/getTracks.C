b0852f4fe94d9f3ce0da7af118e275bcb0ef9455
Helge E. S. Pettersen <helge@krnmpi0.hvl.no>
2022-02-28 09:48:55 +0100

    Added CNN output

--- DTCToolkit/HelperFunctions/getTracks.C
+++ DTCToolkit/HelperFunctions/getTracks.C
@@ -112,7 +112,6 @@ Tracks * loadOrCreateTracks(Bool_t recreate, Int_t Runs, Int_t dataType, Float_t
    
    if (recreate) {
       tracks = getTracksFromClusters(Runs, dataType, kCalorimeter, energy, spotx, spoty, clusters, startRun);
-//      tracks = getTracksFromClustersVisualize(Runs, dataType, kCalorimeter, energy, spotx, spoty, clusters);
       if (tracks) saveTracks(tracks, clusters, dataType, energy);
    }
 
@@ -125,228 +124,6 @@ Tracks * loadOrCreateTracks(Bool_t recreate, Int_t Runs, Int_t dataType, Float_t
       }
    }
    return tracks;
-}
-
-Tracks * getTracksFromClustersVisualize(Int_t Runs, Int_t dataType, Int_t frameType, Float_t energy, Float_t spotx, Float_t spoty, Clusters * saveClusters) {
-   DataInterface   * di = new DataInterface();
-   Int_t             nClusters = kEventsPerRun * 10 * nLayers;
-   Int_t             nTracks = kEventsPerRun * 2;
-   Bool_t            breakSignal = false;
-   Clusters        * clusters = nullptr;
-   Clusters        * clustersCloned = nullptr;
-   TRandom3        * gRandom = new TRandom3(0);
-
-   TH1F * hBefE = new TH1F("hBefE", ";Depth in detector [Layer number];Particle hits per primary", 41, -0.5, 40.5);
-   TH1F * hBefP = new TH1F("hBefP", ";Depth in detector [Layer number];Particle hits per primary", 41, -0.5, 40.5);
-   TH1F * hBefD = new TH1F("hBefD", ";Depth in detector [Layer number];Particle hits per primary", 41, -0.5, 40.5);
-   TH1F * hBefHe3 = new TH1F("hBefHe3", ";Depth in detector [Layer number];Particle hits per primary", 41, -0.5, 40.5);
-   TH1F * hBefT = new TH1F("hBefT", ";Depth in detector [Layer number];Particle hits per primary", 41, -0.5, 40.5);
-   TH1F * hBefHe = new TH1F("hBefHe", ";Depth in detector [Layer number];Particle hits per primary", 41, -0.5, 40.5);
-   TH1F * hAftE = new TH1F("hAftE", ";Depth in detector [Layer number];Particle hits per primary", 41, -0.5, 40.5);
-   TH1F * hAftP = new TH1F("hAftP", ";Depth in detector [Layer number];Particle hits per primary", 41, -0.5, 40.5);
-   TH1F * hAftD = new TH1F("hAftD", ";Depth in detector [Layer number];Particle hits per primary", 41, -0.5, 40.5);
-   TH1F * hAftHe3 = new TH1F("hAftHe3", ";Depth in detector [Layer number];Particle hits per primary", 41, -0.5, 40.5);
-   TH1F * hAftT = new TH1F("hAftT", ";Depth in detector [Layer number];Particle hits per primary", 41, -0.5, 40.5);
-   TH1F * hAftHe = new TH1F("hAftHe", ";Depth in detector [Layer number];Particle hits per primary", 41, -0.5, 40.5);
-
-   TH1F * hCSp = new TH1F("hCSp", "Primary particle cluster;Cluster size;Entries", 50, 0, 50);
-   TH1F * hCSs = new TH1F("hCSs", "Secondary particle cluster;Cluster size;Entries", 50, 0, 50);
-
-   Cluster * thisCluster = nullptr;
-   Int_t nHitsRemovedS = 0;
-   Int_t nHitsRemovedP = 0;
-   Int_t nHitsTotal = 0;
-
-   for (Int_t i=0; i<Runs; i++) {
-      printf("R = %d\n", i);
-      clusters = new Clusters(nClusters);
-      clustersCloned = new Clusters(nClusters);
-      showDebug("Start getMCClusters\n");
-      
-      Hits * hits = new Hits();
-      Hits * hitsCloned = new Hits();
-      Hits * diffusedHits = nullptr;
-//      Hits * diffusedHitsCloned = nullptr;
-      di->getMCClustersThreshold(i, nullptr, hits, spotx, spoty);
-      hits->sortHits(); 
-
-      // DETECTOR RESPONSE THRESHOLD
-      for (Int_t j=0; j<=hits->GetEntriesFast(); j++) {
-         nHitsTotal++;
-         if (hits->At(j)) {
-//            hitsCloned->appendHit(hits->At(j));
-            if (hits->At(j)->getEdep() < 0.1) {
-               if (hits->At(j)->isSecondary()) nHitsRemovedS++;
-               else nHitsRemovedP++;
-               hits->removeHitAt(j);
-            }
-         }
-      }
-      hits->Compress();
-      
-      diffusedHits = diffuseHits(gRandom, hits);
-      diffusedHits->makeLayerIndex();
-      diffusedHits->findClustersFromHits(clusters);
-/*
-      diffusedHitsCloned = diffuseHits(gRandom, hitsCloned);
-      diffusedHitsCloned->makeLayerIndex();
-      diffusedHitsCloned->findClustersFromHits(clustersCloned);
-  */    
-      Cluster *c = nullptr;
-      for (Int_t j=0; j<clusters->GetEntriesFast(); j++) {
-         c = clusters->At(j);
-         if (c->isSecondary()) hCSs->Fill(c->getSize());
-         else hCSp->Fill(c->getSize());
-      }
-
-      // EDEPish CUT
-      if (kHelium) {
-//         clusters->removeSmallClusters(6);
-      }
-      else {
-         clusters->removeSmallClusters(1); // VALIDATE !!!!
-      }
-
-      delete hits;
-      delete diffusedHits;
-//      delete hitsCloned;
-//      delete diffusedHitsCloned;
-/*
-      Int_t layer;
-      for (Int_t j=0; j<clustersCloned->GetEntriesFast(); j++) {
-         thisCluster = clustersCloned->At(j);
-         if (!thisCluster) continue;
-         layer = thisCluster->getLayer();
-         if (thisCluster->getPDG() == 1000020040)  hBefHe->Fill(layer);
-         if (thisCluster->getPDG() == 11)          hBefE->Fill(layer);
-         if (thisCluster->getPDG() == 2212)        hBefP->Fill(layer);
-         if (thisCluster->getPDG() == 1000010020)  hBefD->Fill(layer);
-         if (thisCluster->getPDG() == 1000010030)  hBefT->Fill(layer);
-         if (thisCluster->getPDG() == 1000020030)  hBefHe3->Fill(layer);
-      }
-      
-      for (Int_t j=0; j<clusters->GetEntriesFast(); j++) {
-         thisCluster = clusters->At(j);
-         if (!thisCluster) continue;
-         layer = thisCluster->getLayer();
-         if (thisCluster->getPDG() == 1000020040)  hAftHe->Fill(layer);
-         if (thisCluster->getPDG() == 11)          hAftE->Fill(layer);
-         if (thisCluster->getPDG() == 2212)        hAftP->Fill(layer);
-         if (thisCluster->getPDG() == 1000010020)  hAftD->Fill(layer);
-         if (thisCluster->getPDG() == 1000010030)  hAftT->Fill(layer);
-         if (thisCluster->getPDG() == 1000020030)  hAftHe3->Fill(layer);
-      }
-*/
-      delete clusters;
-//      delete clustersCloned;
-      continue;
-
-   }
-
-   gStyle->SetOptStat(0);
-/*
-   TCanvas *c = new TCanvas("secondaryClusters", "Secondary clusters", 1200, 600);
-   c->Divide(2,1,1e-5,1e-5);
-
-   Float_t tp = Runs * kEventsPerRun;
-   hBefE->Scale(1/tp);
-   hBefP->Scale(1/tp);
-   hBefT->Scale(1/tp);
-   hBefD->Scale(1/tp);
-   hBefHe->Scale(1/tp);
-   hBefHe3->Scale(1/tp);
-   hAftE->Scale(1/tp);
-   hAftP->Scale(1/tp);
-   hAftT->Scale(1/tp);
-   hAftD->Scale(1/tp);
-   hAftHe->Scale(1/tp);
-   hAftHe3->Scale(1/tp);
-
-   printf("Of %d hits in total, %d primaries were removed and %d secondaries were removed by 0.1 kev/um cut\n", nHitsTotal, nHitsRemovedP, nHitsRemovedS);
-   cout << "Relative decrease of He4: " << 100 * ( 1 - hAftHe->Integral() / hBefHe->Integral()) << endl;
-   cout << "Relative decrease of He3: " << 100 * ( 1 - hAftHe3->Integral() / hBefHe3->Integral()) << endl;
-   cout << "Relative decrease of E: " << 100 * ( 1 - hAftE->Integral() / hBefE->Integral()) << endl;
-   cout << "Relative decrease of T: " << 100 * ( 1 - hAftT->Integral() / hBefT->Integral()) << endl;
-   cout << "Relative decrease of D: " << 100 * ( 1 - hAftD->Integral() / hBefD->Integral()) << endl;
-   cout << "Relative decrease of P: " << 100 * ( 1 - hAftP->Integral() / hBefP->Integral()) << endl;
-
-   hBefE->SetLineWidth(3); hBefE->SetLineColor(kMagenta);
-   hBefP->SetLineWidth(3); hBefP->SetLineColor(kBlack);
-   hBefD->SetLineWidth(3); hBefD->SetLineColor(kRed);
-   hBefHe3->SetLineWidth(3); hBefHe3->SetLineColor(kGreen);
-   hBefT->SetLineWidth(3); hBefT->SetLineColor(kBlue);
-   hBefHe->SetLineWidth(3); hBefHe->SetLineColor(kOrange+2);
-
-   hAftE->SetLineWidth(3); hAftE->SetLineColor(kMagenta);
-   hAftP->SetLineWidth(3); hAftP->SetLineColor(kBlack);
-   hAftD->SetLineWidth(3); hAftD->SetLineColor(kRed);
-   hAftHe3->SetLineWidth(3); hAftHe3->SetLineColor(kGreen);
-   hAftT->SetLineWidth(3); hAftT->SetLineColor(kBlue);
-   hAftHe->SetLineWidth(3); hAftHe->SetLineColor(kOrange+2);
-
-   c->cd(1);
-   hBefE->Draw("hist");
-   hBefP->Draw("hist same");
-   hBefD->Draw("hist same");
-   hBefT->Draw("hist same");
-   hBefHe->Draw("hist same");
-   hBefHe3->Draw("hist same");
-  
-   hBefE->GetYaxis()->SetRangeUser(0, 0.75);
-
-   TLegend *leg = new TLegend(0.65, 0.75, 0.98, 0.98);
-   leg->AddEntry(hBefE, "Electrons", "L");
-   leg->AddEntry(hBefP, "Protons", "L");
-   leg->AddEntry(hBefD, "Deuterons", "L");
-   leg->AddEntry(hBefHe3, "Helium3", "L");
-   leg->AddEntry(hBefT, "Tritons", "L");
-   leg->AddEntry(hBefHe, "Helium", "L");
-   leg->SetTextFont(22);
-   leg->Draw();
-
-   c->cd(2);
-   hAftE->Draw("hist");
-   hAftP->Draw("hist same");
-   hAftD->Draw("hist same");
-   hAftT->Draw("hist same");
-   hAftHe->Draw("hist same");
-   hAftHe3->Draw("hist same");
-   hAftE->GetYaxis()->SetRangeUser(0, 0.75);
-   
-   TLegend *leg2 = new TLegend(0.65, 0.75, 0.98, 0.98);
-   leg2->AddEntry(hAftE, "Electrons", "L");
-   leg2->AddEntry(hAftP, "Protons", "L");
-   leg2->AddEntry(hAftD, "Deuterons", "L");
-   leg2->AddEntry(hAftHe3, "Helium3", "L");
-   leg2->AddEntry(hAftT, "Tritons", "L");
-   leg2->AddEntry(hAftHe, "Helium", "L");
-   leg2->SetTextFont(22);
-   leg2->Draw();
-*/
-
-   TCanvas *c2 = new TCanvas("c2", "Cluster sizes", 1200, 600);
-   c2->Divide(2,1,1e-5,1e-5);
-   hCSp->SetLineColor(kBlack); hCSp->SetFillColor(kGray);
-   hCSs->SetLineColor(kBlack); hCSs->SetFillColor(kGreen);
-
-   c2->cd(1);
-   hCSp->Draw();
-   gPad->Update();
-   TLine *l = new TLine(6, 0, 6, gPad->GetUymax());
-   l->SetLineStyle(7);
-   l->Draw();
-
-   c2->cd(2);
-   hCSs->Draw();
-   gPad->Update();
-   l = new TLine(6, 0, 6, gPad->GetUymax());
-   l->SetLineStyle(7);
-   l->Draw();
-
-   delete di;
-   delete gRandom;
-
-   return nullptr;
 }
 
 Tracks * getTracksFromClusters(Int_t Runs, Int_t dataType, Int_t frameType, Float_t energy, Float_t spotx, Float_t spoty, Clusters * saveClusters, Int_t startRun) {
@@ -452,7 +229,7 @@ Tracks * getTracksFromClusters(Int_t Runs, Int_t dataType, Int_t frameType, Floa
 
       if (tracks->GetEntriesFast() == 0) breakSignal = true; // to stop running
 
-      showDebug("Found all the tracks\n");
+      showDebug("Found all the tracks ( " << tracks->GetEntriesFast() << ")\n");
 
       // Track improvements
       Int_t nTracksBefore = 0, nTracksAfter = 0;
@@ -464,8 +241,8 @@ Tracks * getTracksFromClusters(Int_t Runs, Int_t dataType, Int_t frameType, Floa
       
       t3.Start(false);
       tracks->removeEmptyTracks();
-      tracks->removeShortTracks(4); // rem [0, 4]
-      tracks->removeTracksLeavingDetector();
+//      tracks->removeShortTracks(4); // rem [0, 4]
+//      tracks->removeTracksLeavingDetector();
       tracks->fillOutIncompleteTracks(0.4);
       t3.Stop();
       
@@ -486,6 +263,7 @@ Tracks * getTracksFromClusters(Int_t Runs, Int_t dataType, Int_t frameType, Floa
       }
      
       showDebug("append tracks to alltracks...");
+      showDebug(" (" << tracks->GetEntriesFast() << ") tracks\n");
       for (Int_t j=0; j<tracks->GetEntriesFast(); j++) {
          if (!tracks->At(j)) continue;
          allTracks->appendTrack(tracks->At(j));
